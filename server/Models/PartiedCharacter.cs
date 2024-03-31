840c3aa355358daa5e5f0c235e69dfed070b05f0
Ry <OfficiallyRy@outlook.com>
2024-03-31 14:47:14 -0400

    PartiedCharacter model created like a VaultedKeep

---
+++ server/Models/PartiedCharacter.cs
@@ -0,0 +1,10 @@
+namespace easilyswadecsharp.Models
+{
+  public class PartiedCharacter
+  {
+    public int Id { get; set}
+    public string CreatorId { get; set}
+    public int PartyId { get; set}
+    public int CharacterId { get; set}
+  }
+}
